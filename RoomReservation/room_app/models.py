582cbaf98affd5112ad405c7440b56edfba8f4f1
Mihau91 <pierdoot@gmail.com>
2022-07-29 15:21:53 +0200

    created model that keeps info about rooms

--- RoomReservation/room_app/models.py
+++ RoomReservation/room_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class ConferenceRoom(models.Model):
+    room_name = models.CharField(max_length=255)
+    room_capacity = models.PositiveIntegerField()
+    projector_availability = models.BooleanField(default=False)
