a0c455f272e8cbd00d822dccdcc8797ddfaa6430
Mihau91 <pierdoot@gmail.com>
2022-07-31 14:52:34 +0200

    -view AddRoom adds room to database

--- RoomReservation/room_app/views.py
+++ RoomReservation/room_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .models import ConferenceRoom
 
 
 # Create your views here.
@@ -7,6 +8,7 @@ class MainPage(View):
     """
     Renders main page template
     """
+
     def get(self, request):
         return render(request, 'main-page.html')
 
@@ -15,5 +17,26 @@ class AddRoom(View):
     """
     Renders template with form and added data provided by user to the database
     """
+
     def get(self, request):
         return render(request, 'add-room.html')
+
+    def post(self, request):
+        # gets data from form
+        name = request.POST.get("name")
+        capacity = int(request.POST.get("capacity"))
+
+        projector = request.POST.get("projector") == "on"
+
+        # checks if form is properly filled up
+        if not name:
+            return render(request, "add-room.html", context={"error": "Name was not given"})
+        if capacity <= 0:
+            return render(request, "add-room.html", context={"error": "Room capacity cant be less than 0"})
+        if ConferenceRoom.objects.filter(room_name=name).first():  # checks if room with this name already exist
+            return render(request, "add-room.html", context={"error": "Room with this name already exist."})
+
+        # add room to database
+        ConferenceRoom.objects.create(room_name=name, room_capacity=capacity, projector_availability=projector)
+        return redirect("room-list")
+
